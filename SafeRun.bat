492c58f527c922a2650fcf6a5ca555088b74a821
Irish <ongornbk@gmail.com>
2018-09-10 02:30:04 +0200

    v0.08d2

---
+++ SafeRun.bat
@@ -0,0 +1,3 @@
+@echo off
+xcopy /y "D:\STORAGE\Projekty\Diabolo 25\bin\saves\map.save" "D:\STORAGE\Projekty\Diabolo 25\bin\saves\mapbackup.save"
+start USiA.exe
--- copyapp.bat
+++ copyapp.bat
@@ -1,3 +1,2 @@
 @echo off
 xcopy /y "C:\Users\Szymon\Documents\Visual Studio 2017\Projects\USiA\USiA\x64\Release\bin\x64\USiA.exe" "D:\STORAGE\Projekty\Diabolo 25\bin\x64\USiA.exe"
-pause
